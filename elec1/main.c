4eb0c51683ce8f93dedf7471b60f5a68c223798b
Toby Simpson <toby.simpson@usi.ch>
2024-02-05 16:05:56 +0100

    Initial Commit

---
+++ elec1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  elec1
+//
+//  Created by Toby Simpson on 05.02.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
